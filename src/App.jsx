617291c0eb44addd137e527efb2903df5e618c88
Aditya <adityagupta2791@gmail.com>
2026-01-03 15:44:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from './Components/Navbar/Navbar'
+import './App.css'
+import Admin from './Pages/Admin/Admin'
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Admin/>
+    </div>
+  )
+}
+
+export default App
